027155abe1a655073cea95f32d54a0f6b233f67d
Jacob Berg Eriksen <jacob1211@hotmail.com>
2020-03-09 17:13:39 +0100

    added picture (not tested)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	/*
 	code smtp: https://media.pearsoncmg.com/aw/aw_kurose_network_3/labs/lab2/lab2.html
-	code others: https://media.pearsoncmg.com/aw/aw_kurose_network_3/labs/lab2/othercode.html
+    base64 encoding: https://stackoverflow.com/questions/36492084/how-to-convert-an-image-to-base64-string-in-java
 
 	 */
     }
--- src/SMTPConnection.java
+++ src/SMTPConnection.java
@@ -8,36 +8,61 @@ import java.util.*;
  */
 public class SMTPConnection {
     public static void main(String[] args) throws IOException{
-
+        //initializing variables to hold values
         String emailFrom;
         String emailTo;
-        ArrayList<String> text = new ArrayList<>();
+        String subject;
 
+        //(arraylist so that each line of the text can be send seperately)
+        ArrayList<String> textBody = new ArrayList<>();
+
+        //adding picture file
+        File file = new File("hackerman.jpg");
+
+        // string that will hold the picture after it has been encoded.
+        String fileEncoded = "";
+
+        //encoding the file. it will be the "fileEncoded"-string that will be sent.
+        //help from https://stackoverflow.com/questions/36492084/how-to-convert-an-image-to-base64-string-in-java
+        FileInputStream fileReader = new FileInputStream(file);
+        byte[] bytes = new byte[ (int) file.length()];
+        fileReader.read(bytes);
+        fileEncoded = Base64.getEncoder().encodeToString(bytes);
+
+
+        //setting testing to true sends hard coded email with picture
+        //setting testing to false start UI that supports sending mail without picture
         boolean testing = true;
 
         if(testing){
+            //Hardcoded email values for testing
             emailFrom = "whatever@whatever.com";
             emailTo = "jacob1211@hotmail.com";
+            subject = "java mail";
+            textBody.add("hello from line 1");
+            textBody.add("hello from line 2");
+            textBody.add("hello from line 3");
 
-            text.add("hej linje 1");
-            text.add("hej linje 2");
         }
         else {
+            //terminal based interface to input values
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter your email adress");
             emailFrom = scanner.nextLine();
             System.out.println("Enter reciever email adress");
             emailTo = scanner.nextLine();
+            System.out.println("Enter subject of the mail");
+            subject = scanner.nextLine();
             System.out.println("Enter text you want to send. End with \"end\" on a single line");
-            text.add(scanner.nextLine());
-            while (!text.get(text.size() - 1).equals("end")) {
-                text.add(scanner.nextLine());
+            textBody.add(scanner.nextLine());
+            while (!textBody.get(textBody.size() - 1).equals("end")) {
+                textBody.add(scanner.nextLine());
             }
             scanner.close();
         }
 
         SMTPConnection client = new SMTPConnection();
-        client.send(emailFrom, emailTo, text);
+        client.send(emailFrom, emailTo, subject, fileEncoded, textBody);
         client.close();
     }
 
@@ -58,15 +83,13 @@ public class SMTPConnection {
     /* Create an SMTPConnection object. Create the socket and the 
        associated streams. Initialize SMTP connection. */
     public SMTPConnection() throws IOException {
-        connection = new Socket("localhost", SMTP_PORT)/* Fill in */;
-        fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()))/* Fill in */;
-        toServer =   new DataOutputStream(connection.getOutputStream())/* Fill in */;
+        connection = new Socket("localhost", SMTP_PORT);
+        fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        toServer =   new DataOutputStream(connection.getOutputStream());
 
-        /* Fill in */
+
 	/* Read a line from server and check that the reply code is 220.
 	   If not, throw an IOException. */
-        /* Fill in */
-                        //forsøg
         int reply = parseReply(fromServer.readLine()) ;
         if(!(reply == 220)){
             throw new IOException("Reply is not 220");
@@ -74,8 +97,8 @@ public class SMTPConnection {
 
 	/* SMTP handshake. We need the name of the local machine.
 	   Send the appropriate SMTP handshake command. */
-        String localhost = "localhost"/* Fill in */;
-        sendCommand( /* Fill in */ "HELO "+localhost, 250 );
+        String localhost = "localhost";
+        sendCommand("HELO "+ localhost, 250 );
 
         isConnected = true;
     }
@@ -83,19 +106,38 @@ public class SMTPConnection {
     /* Send the message. Write the correct SMTP-commands in the
        correct order. No checking for errors, just throw them to the
        caller. */
-    public void send(String fromMail, String toMail, ArrayList<String> text) throws IOException {
-        /* Fill in */
+    public void send(String fromMail, String toMail, String subject, String fileEncoded, ArrayList<String> text) throws IOException {
+
 	/* Send all the necessary commands to send a message. Call
 	   sendCommand() to do the dirty work. Do _not_ catch the
 	   exception thrown from sendCommand(). */
-        /* Fill in */
         sendCommand("MAIL FROM: <" + fromMail + ">", 250);
         sendCommand("RCPT TO: <" + toMail + ">", 250);
         sendCommand("DATA", 354);
+        sendCommand("To: " + toMail, 0);
+        sendCommand("From: " + fromMail, 0);
+        sendCommand("Subject: " + subject, 0);
+        sendCommand("MIME-version: 1.0",0);
+        sendCommand("Content-Type: multipart/mixed; boundary=seperator",0);
+        sendCommand("",0);
+        sendCommand("--seperator",0);
+        sendCommand("",0);
+
+        //text body
         for(String line : text){
             sendCommand(line,0);
         }
-        sendCommand(".", 250);//telling that message is done.
+
+        //specifying the attached picture and its encoding style
+        sendCommand("--seperator",0);
+        sendCommand("Content-Type:application/octet-stream; name=hackerman.jpg",0);
+        sendCommand("Content-Disposition: attachment; filename=hackerman.jpg",0);
+        sendCommand("Content-Transfer-Encoding: base64",0);
+        //actual encoding
+        sendCommand(fileEncoded,0);
+
+        //Ending message
+        sendCommand(".", 250);
 
     }
 
@@ -118,9 +160,7 @@ public class SMTPConnection {
         /* Fill in */
         /* Write command to server and read reply from server. */
         toServer.writeBytes (command+CRLF);
-        /* Fill in */
 
-        /* Fill in */
 	/* Check that the server's reply code is the same as the parameter
 	   rc. If not, throw an IOException. */
 	    if(rc==0){
@@ -130,12 +170,10 @@ public class SMTPConnection {
 	    if(!(replyC == rc)){
 	        throw new IOException("wrong reply code");
         }
-        /* Fill in */
     }
 
     /* Parse the reply line from the server. Returns the reply code. */
     private int parseReply(String reply) {
-        /* Fill in */
         String code = reply.split(" ")[0];
         return Integer.parseInt(code);
     }
