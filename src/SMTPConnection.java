934309ed1b474e877fe534f05c9804a6a5d7e2de
Jacob Berg Eriksen <jacob1211@hotmail.com>
2020-03-09 14:38:19 +0100

    SMTPConnection should work with text now

--- src/SMTPConnection.java
+++ src/SMTPConnection.java
@@ -7,6 +7,28 @@ import java.util.*;
  *
  */
 public class SMTPConnection {
+    public static void main(String[] args) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your email adress");
+        String emailFrom = scanner.nextLine();
+        System.out.println("Enter reciever email adress");
+        String emailTo = scanner.nextLine();
+        System.out.println("Enter subject of mail");
+        String subject = scanner.nextLine();
+        System.out.println("Enter text you want to send. End with \"end\" on a single line");
+        ArrayList<String> text = new ArrayList<>();
+        text.add(scanner.nextLine());
+        while(!text.get(text.size()-1).equals("end")){
+            text.add(scanner.nextLine());
+        }
+
+        SMTPConnection client = new SMTPConnection();
+
+        client.send(emailFrom, emailTo, text);
+        client.close();
+    }
+
+
     /* The socket to the server */
     private Socket connection;
 
@@ -22,7 +44,7 @@ public class SMTPConnection {
 
     /* Create an SMTPConnection object. Create the socket and the 
        associated streams. Initialize SMTP connection. */
-    public SMTPConnection(Envelope envelope) throws IOException {
+    public SMTPConnection() throws IOException {
         connection = new Socket("localhost", SMTP_PORT)/* Fill in */;
         fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()))/* Fill in */;
         toServer =   new DataOutputStream(connection.getOutputStream())/* Fill in */;
@@ -48,12 +70,20 @@ public class SMTPConnection {
     /* Send the message. Write the correct SMTP-commands in the
        correct order. No checking for errors, just throw them to the
        caller. */
-    public void send(Envelope envelope) throws IOException {
+    public void send(String fromMail, String toMail, ArrayList<String> text) throws IOException {
         /* Fill in */
 	/* Send all the necessary commands to send a message. Call
 	   sendCommand() to do the dirty work. Do _not_ catch the
 	   exception thrown from sendCommand(). */
         /* Fill in */
+        sendCommand("MAIL FROM: <" + fromMail + ">", 250);
+        sendCommand("RCPT TO: <" + toMail + ">", 250);
+        sendCommand("DATA", 354);
+        for(String line : text){
+            sendCommand(line,0);
+        }
+        sendCommand(".", 250);//telling that message is done.
+
     }
 
     /* Close the connection. First, terminate on SMTP level, then
